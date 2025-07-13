8c1ca34a1b415e0082f8f9bbf8ad2a100a4a4bd1
leeyngdo <lyd0531@kaist.ac.kr>
2025-07-13 09:11:12 +0000

    revert changes

--- mj_envs/setup.py
+++ mj_envs/setup.py
@@ -1,24 +1,23 @@
-# Copyright (c) Facebook, Inc. and its affiliates
-# Copyright (c) Rutav Shah, Indian Institute of Technlogy Kharagpur
-
 import os
 import sys
 from setuptools import setup, find_packages
 
-print("Installing RRL. \n Package intended for use with provided conda env. See setup instructions here:")
-
 if sys.version_info.major != 3:
     print("This Python is only compatible with Python 3, but you are running "
           "Python {}. The installation will likely fail.".format(sys.version_info.major))
-
+    
 def read(fname):
     return open(os.path.join(os.path.dirname(__file__), fname)).read()
 
 setup(
-    name='RRL',
+    name='mj_envs',
     version='1.0.0',
     packages=find_packages(),
-    description='RL algorithm for environments in MuJoCo',
-    author='Rutav Shah (Modified by Youngdo Lee)',
-    author_email='lyd0531@kaist.ac.kr',
+    description='environments simulated in MuJoCo',
+    long_description=read('README.md'),
+    url='https://github.com/vikashplus/mj_envs.git',
+    author='Movement Control Lab, UW',
+    install_requires=[
+        'click', 'termcolor',
+    ],
 )
--- mjrl/setup.py
+++ mjrl/setup.py
@@ -1,24 +1,22 @@
-# Copyright (c) Facebook, Inc. and its affiliates
-# Copyright (c) Rutav Shah, Indian Institute of Technlogy Kharagpur
-
 import os
 import sys
 from setuptools import setup, find_packages
 
-print("Installing RRL. \n Package intended for use with provided conda env. See setup instructions here:")
+print("Installing mjrl. \n Package intended for use with provided conda env. See setup instructions here: https://github.com/aravindr93/mjrl/tree/master/setup")
 
 if sys.version_info.major != 3:
     print("This Python is only compatible with Python 3, but you are running "
           "Python {}. The installation will likely fail.".format(sys.version_info.major))
-
+    
 def read(fname):
     return open(os.path.join(os.path.dirname(__file__), fname)).read()
 
 setup(
-    name='RRL',
+    name='mjrl',
     version='1.0.0',
     packages=find_packages(),
-    description='RL algorithm for environments in MuJoCo',
-    author='Rutav Shah (Modified by Youngdo Lee)',
-    author_email='lyd0531@kaist.ac.kr',
+    description='RL algorithms for environments in MuJoCo',
+    long_description=read('README.md'),
+    url='https://github.com/aravindr93/mjrl.git',
+    author='Aravind Rajeswaran',
 )
